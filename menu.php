091b6b23220aa37ab739ef6d8963207fb9e257cc
Carlos Rivera <carlosriveera97@gmail.com>
2016-09-27 16:39:41 -0600

    Implementacion del menu
    Prueba 1
    Hora 4:39

--- menu.php
+++ menu.php
@@ -1,18 +1,13 @@
+
 <div class="navbar-fixed">
-  <nav class="teal darken-3" role="navigation">
-    <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Logo</a>
+  <nav class="nav-wrapper light-blue lighten-1" role="navigation">
+    <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">:v</a>
       <ul class="right hide-on-med-and-down">
-        <li><a href="#">Usuarios</a></li>
-        <li><a href="#">Clientes</a></li>
-        <li><a href="#">Productos</a></li>
-        <li><a href="#">Procesos</a></li>
+        <li><a href="#">Clickea aca prro</a></li>
       </ul>
 
       <ul id="nav-mobile" class="side-nav">
-        <li><a href="#">Usuarios</a></li>
-        <li><a href="#">Clientes</a></li>
-        <li><a href="#">Productos</a></li>
-        <li><a href="#">Procesos</a></li>
+        <li><a href="#">Clickea aca prro</a></li>
       </ul>
       <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
     </div>
