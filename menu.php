15876b430b8e7caf9c3976a15829e2cb03e73670
Carlos Rivera <carlosriveera97@gmail.com>
2016-09-27 16:45:57 -0600

    Actualizacion colores
    Prueba
    Hora 4:45

--- menu.php
+++ menu.php
@@ -1,13 +1,15 @@
 
 <div class="navbar-fixed">
-  <nav class="nav-wrapper light-blue lighten-1" role="navigation">
+  <nav class="nav-wrapper teal accent-4" role="navigation">
     <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">:v</a>
       <ul class="right hide-on-med-and-down">
-        <li><a href="#">Clickea aca prro</a></li>
+        <li><a href="#">Iniciar sesion</a></li>
+        <li><a href="#">Productos</a></li>
       </ul>
 
       <ul id="nav-mobile" class="side-nav">
-        <li><a href="#">Clickea aca prro</a></li>
+        <li><a href="#">Iniciar sesion</a></li>
+        <li><a href="#">Productos</a></li>
       </ul>
       <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
     </div>
