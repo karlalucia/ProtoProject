6d4f71cd8dc7eda33146920b463bac353628d123
Carlos Rivera <carlosriveera97@gmail.com>
2016-10-16 14:16:28 -0600

    Ultima actualización domingo

--- menu.php
+++ menu.php
@@ -1,17 +1,1 @@
 
-<div class="navbar-fixed">
-  <nav class="nav-wrapper teal accent-4" role="navigation">
-    <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">:v</a>
-      <ul class="right hide-on-med-and-down">
-        <li><a href="#">Iniciar sesion</a></li>
-        <li><a href="#">Productos</a></li>
-      </ul>
-
-      <ul id="nav-mobile" class="side-nav">
-        <li><a href="#">Iniciar sesion</a></li>
-        <li><a href="#">Productos</a></li>
-      </ul>
-      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
-    </div>
-  </nav>
-</div>
